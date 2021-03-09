ee22db9b24145b02efdd5ae8d794d4955b30147a
Matheus Veras <matheus.veras@lobofrontal.com.br>
2021-03-09 10:19:08 -0300

    feat(commit inicial)

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StatusBar} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+       
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
